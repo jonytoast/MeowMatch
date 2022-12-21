30a23ac7e25cae3f4daf25244fa4ad2d1afdd0fd
BenjiCCB <bensdecker+CCB@gmail.com>
2022-12-21 18:35:05 -0500

    images working

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,7 @@ function showData(animals){
     var petGenderEl = document.createElement('div');
     petGenderEl.textContent = animals[i].gender;
 
-    if (animals[i].primary_photo_cropped.small){
+    if (animals[i].primary_photo_cropped){
       var petImgEl = document.createElement('img');
       petImgEl.setAttribute("src", animals[i].primary_photo_cropped.small);
       petBoxEl.appendChild(petImgEl);
