221cd321243b6909e930f4b15943678d89becb27
BenjiCCB <bensdecker+CCB@gmail.com>
2022-12-21 14:09:50 -0500

    form playing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
 var startButton = document.querySelector('.start-button');
 
+var searchFormEl = document.querySelector('.search-form');
+var searchBoxEl = document.querySelector('.search-box');
+var selectionEl = document.querySelector('.select1');
+
+
 function startUserFlow(){
     console.log("i've started")
 }
 
 function getData(){
-
   var pf = new petfinder.Client({apiKey: "8xTjKkX9rqOoNSgYVcICbmHSHx7E8NcVyYx0pXUxWTPBB8RzJG", secret: "7B3fC5cIclR0jWTEliyi7cM52VgwzLrPm382rKwe"});
 
   pf.animal.search()
@@ -18,12 +22,38 @@ function getData(){
     });
 }
 
-
 function showData(animals){
   for (var i = 0; i < animals.length; i++) {  
       console.log(animals[i].name)
   }
 }
 
+
+// Form
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  var searchContent = searchBoxEl.value.trim();
+  var selectContent = selectionEl.value.trim();
+
+  console.log(searchContent + " " + selectContent);
+
+  searchBoxEl.value = "";
+  selectionEl.value = "";
+
+  // if (searchCity) {
+  //   updateHistoryButtons(searchCity);
+  //   getForecastData(searchCity);
+
+  //   forecastContainerEl.textContent = '';
+  //   cityInputEl.value = '';
+
+  // } else {
+  //   alert('Please enter a valid city name');
+  // }
+};
+
+
 getData();
 startButton.addEventListener('click', startUserFlow);
+searchFormEl.addEventListener('submit', formSubmitHandler);
